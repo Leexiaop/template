aaa91325f9c7a9ca7b56c842218dc979dcd73afd
lixiaopeng06 <lixiaopeng06.baidu.com>
2023-04-12 23:37:35 +0800

    配置h5模版

---
+++ config/routes.js
@@ -0,0 +1,29 @@
+export default [
+  {
+    key: '1',
+    name: 'login',
+    path: '/login',
+    component: '@/pages/login',
+  },
+  {
+    key: '2',
+    name: 'index',
+    path: '/',
+    component: '@/pages',
+    wrappers: ['@/wrappers/auth'],
+  },
+  {
+    key: '3',
+    path: '/home',
+    name: 'home',
+    component: '@/pages/home',
+    wrappers: ['@/wrappers/auth'],
+  },
+  {
+    key: '4',
+    name: 'about',
+    path: '/about',
+    component: '@/pages/about',
+    wrappers: ['@/wrappers/auth'],
+  },
+];
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,108 +1,27 @@
-import {useEffect, useState} from 'react';
-import {ConfigProvider, Layout, Menu, Avatar, Space, Dropdown} from 'antd';
-import zhCN from 'antd/lib/locale/zh_CN';
-import {connect} from 'dva';
-import {UserOutlined, SlackSquareFilled, DownOutlined} from '@ant-design/icons';
-// import logo from '../assets/texture/logo.png';
-import {useHistory} from 'umi';
-import menu from '@/routers/routers';
+import { connect } from 'dva';
 import './index.less';
 
-const {Header, Content, Sider} = Layout;
-
 const Index = (props) => {
-    const {children, loginUser} = props;
-    const [selectedKey, setSelectedKey] = useState('1');
-    const history = useHistory();
-    useEffect(() => {
-        setSelectedKey(menu.find(item => history.location.pathname.indexOf(item.path) > -1)?.key);
-    }, [history.location.pathname]);
-
-    const onMenuChange = e => {
-        setSelectedKey(e.key);
-        history.push(menu.find(item => item.key === e.key)?.path);
-    };
-    return (
-        <ConfigProvider locale={zhCN}>
-            <Layout>
-                <Header>
-                    <Space
-                        style={{
-                            width: '100%',
-                            color: '#fff',
-                            display: 'flex',
-                            justifyContent: 'space-between'
-                        }}
-                    >
-                        <Avatar
-                            size="16"
-                            style={{
-                                backgroundColor: '#87d068'
-                            }}
-                            shape="square"
-                            icon={<SlackSquareFilled />}
-                        />
-                        <Space>
-                            <Avatar
-                                style={{
-                                    backgroundColor: '#87d068'
-                                }}
-                                icon={<UserOutlined />}
-                            />
-                            <Dropdown
-                                menu={
-                                    [{key: 1, label: '退出'}]
-                                }
-                            >
-                                <a onClick={(e) => e.preventDefault()}>
-                                    <Space>
-                                        {loginUser.name}
-                                        <DownOutlined />
-                                    </Space>
-                                </a>
-                            </Dropdown>
-                        </Space>
-                    </Space>
-                </Header>
-                <Layout>
-                    <Sider
-                        style={{
-                            overflow: 'scroll'
-                        }}
-                    >
-                        <Menu
-                            theme="dark"
-                            mode="inline"
-                            selectedKeys={selectedKey}
-                            items={menu}
-                            onClick={onMenuChange}
-                        />
-                    </Sider>
-                    <Content
-                        style={{
-                            overflow: 'scroll',
-                            padding: 24
-                        }}>
-                        {children}
-                    </Content>
-                </Layout>
-            </Layout>
-        </ConfigProvider>
-    );
+  const { loginUser } = props;
+  return (
+    <div className="index">
+      快看看
+      {loginUser.name}
+    </div>
+  );
 };
 
-const mapStateToProps = ({state}) => {
-    return {
-        loginUser: state.loginUser
-    };
+const mapStateToProps = ({ state }) => {
+  return {
+    loginUser: state.loginUser,
+  };
 };
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        setData(data) {
-            dispatch({type: 'state/setData', data});
-        }
-    };
+  return {
+    setData(data) {
+      dispatch({ type: 'state/setData', data });
+    },
+  };
 };
 export default connect(mapStateToProps, mapDispatchToProps)(Index);
-
