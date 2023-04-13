7908810f7c382a4034e4432b358eb5c809257d18
lixiaopeng06 <lixiaopeng06.baidu.com>
2023-04-13 11:36:18 +0800

    h5

--- src/app.js
+++ src/app.js
@@ -1,8 +1,1 @@
 import 'lib-flexible';
-
-export function patchRoutes({routes}) {
-    console.log(routes, 555);
-}
-export function onRouteChange({location, routes, action}) {
-    console.log(location, 222);
-}
