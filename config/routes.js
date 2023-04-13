32b3da211ff9ebf57d3fa98833c88ff8564df486
lixiaopeng06 <lixiaopeng06.baidu.com>
2023-04-13 15:23:20 +0800

    h5

--- config/routes.js
+++ config/routes.js
@@ -1,13 +1,11 @@
 export default [
     {
         key: '1',
-        name: 'login',
         path: '/login',
         component: '@/pages/login'
     },
     {
         key: '2',
-        name: 'index',
         path: '/',
         component: '@/pages',
         wrappers: ['@/wrappers/auth']
@@ -15,13 +13,11 @@ export default [
     {
         key: '3',
         path: '/home',
-        name: 'home',
         component: '@/pages/home',
         wrappers: ['@/wrappers/auth']
     },
     {
         key: '4',
-        name: 'about',
         path: '/about',
         component: '@/pages/about',
         wrappers: ['@/wrappers/auth']
